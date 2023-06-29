d0c933ea9354ad81eb19f6515d75475fc6062252
Admin <Admin@DESKTOP-VMBDM6V>
2023-06-29 20:42:42 +0530

    added controller

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class DemoController {
+
+	@GetMapping("/say-hello")
+	public String sayHello() {
+		return "Hello! from Zaheer";
+	}
+}
